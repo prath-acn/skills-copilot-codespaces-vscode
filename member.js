87cc1140f36aef0e8c342cc2207207224b498bc8
prath-acn <132379521+prath-acn@users.noreply.github.com>
2025-02-20 16:18:06 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    let skills = ['JavaScript', 'React', 'Node.js', 'Express.js'];
+    let member = {
+        name: 'John Doe',
+        age: 25,
+        skills: skills
+    }
+    return member;
+}
